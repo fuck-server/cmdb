24b4f9a4fcfd0c02ca2a94132fab30b06d4cfbfe
chenyk1219 <150651687+chenyk1219@users.noreply.github.com>
2024-01-09 21:58:03 +0800

    update: use chine time time: 2024年1月9日 21:57:37

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -103,13 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'zh-hans'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
+
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_L10N = True
 
 
 # Static files (CSS, JavaScript, Images)
